c7319731e8e0be32f01de3e671ddfeffe3fe93f4
tboy4all <taiyesuleiman09@gmail.com>
2024-02-16 13:14:34 +0100

    Build Issue Tracker using NextJS

---
+++ app/NavBar.tsx
@@ -0,0 +1,45 @@
+'use client'
+
+import Link from 'next/link'
+import React from 'react'
+import { AiFillBug } from 'react-icons/ai'
+import classnames from 'classnames'
+import { usePathname } from 'next/navigation'
+
+const NavBar = () => {
+  const currentPath = usePathname()
+  const links = [
+    {
+      label: 'Dashboard',
+      href: '/',
+    },
+    {
+      label: 'Issues',
+      href: '/issues',
+    },
+  ]
+  return (
+    <nav className='flex space-x-6 border-b mb-5 h-14 px-5 items-center'>
+      <Link href='/'>
+        <AiFillBug />
+      </Link>
+      <ul className='flex space-x-6'>
+        {links.map((link) => (
+          <Link
+            key={link.href}
+            href={link.href}
+            className={classnames({
+              'text-zinc-900': link.href === currentPath,
+              'text-zinc-500': link.href !== currentPath,
+              'hover:text-zinc-800 transition-colors': true,
+            })}
+          >
+            {link.label}
+          </Link>
+        ))}
+      </ul>
+    </nav>
+  )
+}
+
+export default NavBar
---
+++ app/issues/new/page.tsx
@@ -0,0 +1,42 @@
+'use client'
+
+import { Button, TextField } from '@radix-ui/themes'
+import SimpleMDE from 'react-simplemde-editor'
+import { useForm, Controller } from 'react-hook-form'
+import axios from 'axios'
+import 'easymde/dist/easymde.min.css'
+import { useRouter } from 'next/navigation'
+
+interface IssueForm {
+  title: string
+  description: string
+}
+
+const NewIssuePage = () => {
+  const router = useRouter()
+  const { register, control, handleSubmit } = useForm<IssueForm>()
+
+  return (
+    <form
+      className='max-w-xl space-y-3'
+      onSubmit={handleSubmit(async (data) => {
+        await axios.post('/api/issues', data)
+        router.push('/issues ')
+      })}
+    >
+      <TextField.Root>
+        <TextField.Input placeholder='Title' {...register('title')} />
+      </TextField.Root>
+      <Controller
+        name='description'
+        control={control}
+        render={({ field }) => (
+          <SimpleMDE placeholder='Description' {...field} />
+        )}
+      />
+      <Button>Submit New Issue</Button>
+    </form>
+  )
+}
+
+export default NewIssuePage
