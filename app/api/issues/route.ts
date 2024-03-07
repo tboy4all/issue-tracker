2f51b7faa99355b21f54665ba902361072ff0472
tboy4all <taiyesuleiman09@gmail.com>
2024-03-07 11:43:35 +0100

    Handle errors

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -6,8 +6,8 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 const createIssueSchema = z.object({
-  title: z.string().min(1).max(255),
-  description: z.string().min(1),
+  title: z.string().min(1, 'Title is required.').max(255),
+  description: z.string().min(1, 'Description is required'),
 })
 
 export async function POST(request: NextRequest) {
@@ -15,7 +15,8 @@ export async function POST(request: NextRequest) {
   const validation = createIssueSchema.safeParse(body)
 
   if (!validation.success) {
-    return NextResponse.json(validation.error.errors, { status: 400 })
+    // return NextResponse.json(validation.error.errors, { status: 400 })
+    return NextResponse.json(validation.error.format(), { status: 400 })
   }
 
   const newIssue = await prisma.issue.create({
