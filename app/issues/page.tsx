47cd318ee3ac537fd6b10b58ece7f8b61eef6cf7
tboy4all <taiyesuleiman09@gmail.com>
2024-03-08 11:45:02 +0100

    Build a styled link component

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,10 +1,11 @@
 import { PrismaClient } from '@prisma/client'
-import { Button, Table } from '@radix-ui/themes'
-import Link from 'next/link'
+import { Table } from '@radix-ui/themes'
+// import Link from 'next/link'
 import React from 'react'
 import IssueStatusBadge from '../components/IssueStatusBadge'
 import delay from 'delay'
 import IssueActions from './IssueActions'
+import Link from '../components/Link'
 
 const prisma = new PrismaClient()
 
