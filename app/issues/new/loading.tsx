53f5a2d6fb65a2b54d4a286db0cf0562b49ec61a
tboy4all <taiyesuleiman09@gmail.com>
2024-03-08 12:11:17 +0100

    Disable SSR for loading markdown editor

--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -7,7 +7,7 @@ const LoadingNewIssuePAge = () => {
   return (
     <Box className='max-w-xl'>
       <Skeleton />
-      <Skeleton height='20rem' />
+      <Skeleton height='20rem ' />
     </Box>
   )
 }
