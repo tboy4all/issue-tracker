a6811516508d045811c33bbfb6420409933a8e23
tboy4all <taiyesuleiman09@gmail.com>
2024-04-25 11:27:58 +0100

    Lay out Dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { Flex, Grid } from '@radix-ui/themes'
 import Pagination from './components/Pagination'
 import IssueChart from './IssueChart'
 import IssueSummary from './IssueSummary'
@@ -19,5 +20,14 @@ export default async function Home() {
     where: { status: 'CLOSED' },
   })
 
-  return <IssueChart open={open} inProgress={inProgress} closed={closed} />
+  return (
+    <Grid columns={{ initial: '1', md: '2' }} gap='5'>
+      <Flex direction='column' gap='5'>
+        <IssueSummary open={open} inProgress={inProgress} closed={closed} />
+        <IssueChart open={open} inProgress={inProgress} closed={closed} />
+      </Flex>
+
+      <LatestIssues />
+    </Grid>
+  )
 }
